b89bbf5b67e7a5f3604c671e559b48c59a66ad04
Stavros Kois <s.kois@outlook.com>
2026-02-10 13:17:12 +0200

    fix shadowing

--- backend/internal/shared/api/middleware_requestid.go
+++ backend/internal/shared/api/middleware_requestid.go
@@ -14,13 +14,13 @@ func (m *MiddlewareHandler) RequestIDMiddleware(next http.Handler) http.Handler 
 		// Get or generate request ID
 		requestID := r.Header.Get(RequestIDHeader)
 		if requestID == "" {
-			uuid, err := uuid.NewV7()
+			reqID, err := uuid.NewV7()
 			if err != nil {
 				m.l.Error("failed to generate request ID", utils.ErrAttr(err))
 				http.Error(w, "Service temporarily unavailable", http.StatusServiceUnavailable)
 				return
 			}
-			requestID = uuid.String()
+			requestID = reqID.String()
 		}
 
 		w.Header().Set(RequestIDHeader, requestID)
